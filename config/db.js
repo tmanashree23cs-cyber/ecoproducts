c1dbd369c83a293d25c7fbd3adaa41b3e4a6b132
snehathakur149 <75410708+snehathakur149@users.noreply.github.com>
2026-02-18 03:25:19 +0530

    Show db error

--- config/db.js
+++ config/db.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if (err) {
-        console.log("Database connection failed");
+        console.log("Database connection failed:", err);
     } else {
         console.log("Connected to MySQL");
     }
